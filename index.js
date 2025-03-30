d0e43cd0bf87909fde700e08d9a976e01f583691
agung <muhammadagunghartono@gmail.com>
2025-03-30 13:53:05 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
-const DATA_FILE = 'data.json';
+const DATA_FILE = '/tmp/data.json';
 
 app.use(cors());
 app.use(express.json());
